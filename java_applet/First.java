d85f4c247d0a3b2c8c060c6704afb18a905b0e64
pavankumar19992208 <pavankumar19992208@gmail.com>
2022-10-05 15:21:11 +0530

    java

---
+++ java_applet/First.java
@@ -0,0 +1,15 @@
+import java.applet.Applet;  
+import java.awt.Graphics;  
+public class First extends Applet{  
+x(){ 
+public void paint(Graphics g){  
+g.drawString("welcome to applet",150,150);  
+} 
+}
+public static void main(String...s)
+{
+new x();
+}
+} 
+  
+}  
